13903a3205921edd607c341dac5dfde46e22b209
Default2882 <soumyasaurav@gmail.com>
2024-07-04 18:10:40 +0530

    Copied TimePickerDialog from source, since it is not available in beta. Implemented Timer with state.

--- app/src/main/java/com/appblocker/TimePickerDialog.kt
+++ app/src/main/java/com/appblocker/TimePickerDialog.kt
@@ -1,39 +1,83 @@
-package com.appblocker
-
 import android.widget.TimePicker
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.appblocker.ui.theme.AppBLockerTheme
 
+/**
+ * The TimePickerDialog is not included in the M3 package (1.1.0-beta01). You can use:
+ * https://stackoverflow.com/questions/75853449/timepickerdialog-in-jetpack-compose
+ */
 @Composable
 fun TimePickerDialog(
-    title: String = "Pick Your Time",
-    onSubmit: () -> Unit,
-    onDismiss: () -> Unit,
+    title: String = "Select Time",
+    onCancel: () -> Unit,
+    onConfirm: () -> Unit,
     toggle: @Composable () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
     Dialog(
-        onDismissRequest = onDismiss,
+        onDismissRequest = onCancel,
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
     ) {
-        Column {
-            Text(text = title)
-            content()
-            Row {
-                TextButton(onClick = onDismiss) {
-                    Text(text = "Dismiss")
-                }
-                TextButton(onClick = onSubmit) {
-                    Text(text = "Submit")
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    toggle()
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(
+                        onClick = onCancel
+                    ) { Text("Cancel") }
+                    TextButton(
+                        onClick = onConfirm
+                    ) { Text("OK") }
                 }
             }
         }
@@ -41,12 +85,12 @@ fun TimePickerDialog(
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun TimePickerDialogPreview() {
     val state = rememberTimePickerState()
     AppBLockerTheme {
-        TimePickerDialog("Pick Your Time", {}, {}) {
+        TimePickerDialog(onCancel = { /*TODO*/ }, onConfirm = { /*TODO*/ }) {
             TimePicker(state = state)
         }
     }
--- app/src/main/java/com/appblocker/Timer.kt
+++ app/src/main/java/com/appblocker/Timer.kt
@@ -1,10 +1,11 @@
 package com.appblocker
 
+import TimePickerDialog
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberTimePickerState
@@ -12,38 +13,66 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.appblocker.ui.theme.AppBLockerTheme
-import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Locale
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Timer(modifier: Modifier = Modifier) {
-    var showTimePicker by remember { mutableStateOf(false) }
-    val state = rememberTimePickerState()
-    val formatter = remember { SimpleDateFormat("hh:mm a", Locale.getDefault()) }
-    val snackState = remember { SnackbarHostState() }
-    val snackScope = rememberCoroutineScope()
+    var showStartTimePicker by remember { mutableStateOf(false) }
+    var showEndTimePicker by remember { mutableStateOf(false) }
+    
+    val time = Calendar.getInstance()
+    time.timeInMillis = System.currentTimeMillis()
+    
+    val startTimePickerState = rememberTimePickerState(
+        initialMinute = time[Calendar.MINUTE],
+        initialHour = time[Calendar.HOUR]
+    )
+
+    val endTimePickerState = rememberTimePickerState(
+        initialMinute = time[Calendar.MINUTE],
+        initialHour = time[Calendar.HOUR]
+    )
+
+    fun hideStartTimePicker() {
+        showStartTimePicker = false
+    }
+
+    fun hideEndTimePicker() {
+        showEndTimePicker = false
+    }
 
     Box {
-        Button(modifier = Modifier.align(Alignment.Center), onClick = { showTimePicker = true }) {
-            Text("Set Time")
+        Surface {
+            Column {
+                Button(onClick = { showStartTimePicker = true }) {
+                    Text("Set Start Time")
+                }
+                Button(onClick = { showEndTimePicker = true }) {
+                    Text("Set End Time")
+                }
+            }   
         }
-        SnackbarHost(hostState = snackState)
     }
 
-    if (showTimePicker) {
-//        TimePicker(state = state)
+    if (showStartTimePicker) {
+        TimePickerDialog(
+            onCancel = { hideTimePicker() },
+            onConfirm = { hideTimePicker() }
+        ) {
+            TimePicker(state = startTimePickerState)
+        }
     }
+}
+
+@Composable
+fun TimePicker(modifier: Modifier = Modifier) {
+    
 }
 
 @Preview(showBackground = true)
